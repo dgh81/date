3047a9764c15e776b95ba7f14ee6c00e6e02d7b6
Daniel <danielguldberg@gmail.com>
2021-09-07 10:13:07 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String date = "Monday";
+    int day = 6;
+    String month = "September";
+    int year = 2021;
+        System.out.println(date);
+        System.out.println(day);
+        System.out.println(month);
+        System.out.println(year);
+        System.out.println(date+", "+month+" "+day+" "+year);
+        System.out.println(date+" "+day+" "+month+" "+year);
+    }
+}
